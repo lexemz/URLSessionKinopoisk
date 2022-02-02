50e2f6ae2b921b0a7376982e6f41b9868ffb7fc1
Igor Buzykin <lexemz@gmail.com>
2022-02-02 19:21:27 +0300

    Initial Commit

---
+++ URLSessionKinopoisk/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  URLSessionKinopoisk
+//
+//  Created by Igor on 02.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
